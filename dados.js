6b42863c8abb3a997b20fbf9a7425c74c98c0688
taciofsantos <tacio.fernandes@hotmail.com>
2024-09-06 10:48:54 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        nome : "Silvio Santos",
+        descricao : "Silvio Santos foi muito mais do que um apresentador de televisão. Ele foi um verdadeiro fenômeno da comunicação, capaz de conquistar multidões com seu carisma, humor e espontaneidade.",
+        link: "https://pt.wikipedia.org/wiki/Silvio_Santos",
+        tags: "TV tv baú bau dinheiro sbt empresario empresário"
+    },
+    {
+        nome: "Fausto Silva",
+        descricao: "Fausto Silva, conhecido como Faustão, é um apresentador de televisão brasileiro, famoso por seus programas de auditório e por sua longa trajetória na Rede Globo.",
+        link: "https://pt.wikipedia.org/wiki/Fausto_Silva",
+        tags:"globo rádio radio domingao domingão domingo"
+    },
+    {
+        nome: "Raul Gil",
+        descricao: "Raul Gil é um nome que dispensa apresentações na televisão brasileira. Com uma carreira que se estende por décadas, ele conquistou o público de todas as idades com seus programas repletos de alegria, música e diversão.",
+        link: "https://pt.wikipedia.org/wiki.Raul_Gil",
+        tags:"crianças crianca banquinho chapéu chapeu"
+    }
+];
